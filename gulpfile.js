7db1bd7297912295f7883363218b9b345dfaae32
Davyd McColl <davydm@gmail.com>
2020-05-04 22:14:07 +0200

    :tada: modernise & drop gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,23 @@ var gulp = require('gulp'),
 	filter = require('gulp-filter'),
 	mocha = require('gulp-mocha');
 
-gulp.task('default', ['test']);
-
-gulp.task('watch', function () {
-	gulp.watch(['**/*.js', '!node_modules/**/*.js'], ['test']);
-});
-
-gulp.task('test', ['lint'], function () {
-	gulp.src('test/*.js', {read: false})
-		.pipe(mocha({reporter: 'spec', ui: 'bdd'}));
-});
-
 gulp.task('lint', function () {
 	return gulp.src('**/*.js')
 		.pipe(filter(['*', '!node_modules/**/*']))
 		.pipe(jshint({node: true}))
 		.pipe(jshint.reporter('default'));
 });
+
+gulp.task('test', gulp.series('lint'), function () {
+	gulp.src('test/*.js', {read: false})
+		.pipe(mocha({reporter: 'spec', ui: 'bdd'}));
+});
+
+gulp.task('default', gulp.series('test'), function() {
+	return Promise.resolve();
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['**/*.js', '!node_modules/**/*.js'], ['test']);
+});
+
